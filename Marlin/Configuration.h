b2589c31b4b3983a442864959bbb6032f6923a41
KG-3D <kayogaldino@gmail.com>
2026-03-21 15:43:47 -0300

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1160,7 +1160,9 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 3 
+#define PROBING_MARGIN 3
+#define PROBING_MARGIN_FRONT 20
+  
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (160*60)//(80*60)
