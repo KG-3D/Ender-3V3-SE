beae7327805ae8669a36abbced8a0d6cae971074
KG-3D <kayogaldino@gmail.com>
2026-05-05 10:03:31 -0300

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1167,11 +1167,11 @@
 #define XY_PROBE_FEEDRATE (200*60)//(200*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (2.5*60)
+#define Z_PROBE_FEEDRATE_FAST (150)
 // #define Z_PROBE_FEEDRATE_FAST (4*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_FEEDRATE_SLOW (2.5*60)
+#define Z_PROBE_FEEDRATE_SLOW (150)
 
 /**
  * Probe Activation Switch
