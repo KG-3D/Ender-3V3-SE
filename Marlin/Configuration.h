5fa2e3137bb6ed7fa6596e9ff140cc5e81bdbe4b
KG-3D <kayogaldino@gmail.com>
2026-05-10 21:13:22 -0300

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1845,11 +1845,11 @@
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED    255 // Value from 0 to 255
 
-// #define PREHEAT_2_LABEL       "TPU"
-// #define PREHEAT_2_TEMP_HOTEND 230
-// #define PREHEAT_2_TEMP_BED     70
-// #define PREHEAT_2_TEMP_CHAMBER 35
-// #define PREHEAT_2_FAN_SPEED    0//255 // Value from 0 to 255
+#define PREHEAT_2_LABEL       "ABS"
+#define PREHEAT_2_TEMP_HOTEND 220
+#define PREHEAT_2_TEMP_BED     90
+#define PREHEAT_2_TEMP_CHAMBER 35
+#define PREHEAT_2_FAN_SPEED    0//255 // Value from 0 to 255
 
 // #define PREHEAT_3_LABEL       "PETG"
 // #define PREHEAT_3_TEMP_HOTEND 240
@@ -1857,11 +1857,11 @@
 // #define PREHEAT_3_TEMP_CHAMBER 35
 // #define PREHEAT_3_FAN_SPEED    0//255 // Value from 0 to 255
 
-#define PREHEAT_4_LABEL       "ABS"
-#define PREHEAT_4_TEMP_HOTEND 220
-#define PREHEAT_4_TEMP_BED    90
-#define PREHEAT_4_TEMP_CHAMBER 35
-#define PREHEAT_4_FAN_SPEED    0 // Value from 0 to 255
+// #define PREHEAT_4_LABEL       "ABS"
+// #define PREHEAT_4_TEMP_HOTEND 220
+// #define PREHEAT_4_TEMP_BED    90
+// #define PREHEAT_4_TEMP_CHAMBER 35
+// #define PREHEAT_4_FAN_SPEED    0 // Value from 0 to 255
 
 /**
  * Nozzle Park
