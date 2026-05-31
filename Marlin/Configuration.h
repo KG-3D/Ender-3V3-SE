5cc99cdab3531ada0ea81ce08d5ed9f67e651e67
KG-3D <kayogaldino@gmail.com>
2026-05-31 13:22:43 -0300

    Adjust probing feedrates and configurations

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1167,7 +1167,7 @@
 #define XY_PROBE_FEEDRATE (200*60)//(200*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (2*60)
+#define Z_PROBE_FEEDRATE_FAST (8*60)
 // #define Z_PROBE_FEEDRATE_FAST (4*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
@@ -1210,7 +1210,7 @@
  * A total of 3 or more adds more slow probes, taking the average.
  */
 #define MULTIPLE_PROBING   2
-#define EXTRA_PROBING    1
+#define EXTRA_PROBING    0
 
 /**
  * Z probes require clearance when deploying, stowing, and moving between
@@ -1362,7 +1362,7 @@
 #define X_MIN_POS -11
 #define Y_MIN_POS -17
 
-#define Z_MIN_POS -0.2
+#define Z_MIN_POS 0
 // #if ENABLED(WUHAN_CHENGE_PLATFORM)  //武汉改结构
   #define X_MAX_POS X_BED_SIZE
 //#else
@@ -1583,7 +1583,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 9
+  #define GRID_MAX_POINTS_X 7
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
