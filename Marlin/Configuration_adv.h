34392fd2bb6f47a86abd8b20c9d15fd974c467b7
KG-3D <kayogaldino@gmail.com>
2026-06-03 18:06:34 -0300

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -884,7 +884,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.
-  #define TRAMMING_POINT_XY { {  40, 40 }, { 180,  40 }, { 180, 160 }, { 40, 160 } }
+  #define TRAMMING_POINT_XY { {  35, 20 }, { 200,  20 }, { 200, 190 }, { 35, 190 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
